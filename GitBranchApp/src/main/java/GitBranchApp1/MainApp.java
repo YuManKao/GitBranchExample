7cec1611ea0630ffade28ff692a9b9d2f2a3f416
LAPTOP-PATF7J21\026 <lmt860219@gmail.com>
2023-01-07 14:50:38 +0800

    handsome

--- GitBranchApp/src/main/java/GitBranchApp1/MainApp.java
+++ GitBranchApp/src/main/java/GitBranchApp1/MainApp.java
@@ -2,16 +2,30 @@ package GitBranchApp1;
 
 public class MainApp {
 	public static void main(String[] args) {
-		// 組長
+		// 蝯
 		System.out.println("1st Version");
-		// 組長(新增)
+		// 蝯(�憓�)
 		Math cMath = new Math();
 		System.out.println();
 		System.out.println(cMath.add(10, 20));
+		
+		
+		
+		
+		//帥哥
+		Handsome hh=new Handsome()
+				
+		System.out.println(hh.handsome("213213"));
 	}
 }
 class Math {
 	int add(int a, int b) {
 		return a + b;
+	}
+}
+class Handsome{
+	private String s="大腳";
+	int handsome(String str) {
+		return  str+s;
 	}
 }
